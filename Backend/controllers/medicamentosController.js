f73c43394a9f7bd5e2b43b74f51b5636aca692e3
Franco Drachenberg <francodrach@gmail.com>
2025-06-21 05:18:16 -0300

    fix getMedicamentoById

--- Backend/controllers/medicamentosController.js
+++ Backend/controllers/medicamentosController.js
@@ -24,31 +24,37 @@ const getMedicamentosConCategorias = (req, res) => {
 };
 
 const getMedicamentoById = (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
 
   if (!id) {
     return res.status(400).json({ message: "ID is required." });
   }
 
-  const query = `
-        call get_medicamentos_by_id(?);
-    `;
+  const query = `CALL get_medicamentos_by_id(?)`;
 
-  connection.query(query, (error, results) => {
+  connection.query(query, [id], (error, results) => {
     if (error) {
-      console.error("Error fetching medications:", error);
+      console.error("Error fetching medication by ID:", error);
       return res.status(500).json({
-        message: "Error fetching medications from the database.",
+        message: "Error fetching medication from the database.",
       });
     }
 
-    const formattedResults = results.map((medicamento) => ({
-      ...medicamento,
-      Venta_libre: Boolean(medicamento.Venta_libre),
-      categorias: JSON.parse(medicamento.categorias),
-    }));
+    const medicationData = results[0];
 
-    res.status(200).json(formattedResults);
+    if (!medicationData || medicationData.length === 0) {
+      return res
+        .status(404)
+        .json({ message: `Medicamento with ID ${id} not found.` });
+    }
+
+    const formattedResult = {
+      ...medicationData[0],
+      Venta_libre: Boolean(medicationData[0].Venta_libre),
+      categorias: JSON.parse(medicationData[0].categorias),
+    };
+
+    res.status(200).json(formattedResult);
   });
 };
 
