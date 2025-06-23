e3f407c1baea4a1c680bbd1aae05787f6ef8bcf1
IgnacioDiazV <nachodiazvaldez@gmail.com>
2025-06-23 02:01:13 -0300

    Faq

--- Frontend/src/components/footer.jsx
+++ Frontend/src/components/footer.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-col">
         <h3>ATENCIÓN AL CLIENTE</h3>
         <ul>
-          <li>Preguntas frecuentes</li>
+          <li><Link to="/faq">Preguntas frecuentes</Link></li>
           <li>Empresa</li>
           <li>Obras sociales</li>
         </ul>
@@ -12,28 +14,11 @@ export default function Footer() {
       <div className="footer-col">
         <h3>CONTACTO</h3>
         <div className="footer-social">
-          <a href="#">
-            <i className="fab fa-facebook-f icon facebook"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-instagram icon instagram"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-whatsapp icon whatsapp"></i>
-          </a>
+          <a href="#"><i className="fab fa-facebook-f icon facebook"></i></a>
+          <a href="#"><i className="fab fa-instagram icon instagram"></i></a>
+          <a href="#"><i className="fab fa-whatsapp icon whatsapp"></i></a>
         </div>
       </div>
-
-      {/* <div className="footer-col footer-form">
-        <h3>ESCRIBÍNOS</h3>
-        <form>
-          <input type="text" placeholder="Nombre y Apellido" />
-          <input type="text" placeholder="Telefono" />
-          <input type="email" placeholder="Email" />
-          <textarea placeholder="Mensaje"></textarea>
-          <button type="submit">ENVIAR</button>
-        </form>
-      </div> */}
     </footer>
   );
 }
